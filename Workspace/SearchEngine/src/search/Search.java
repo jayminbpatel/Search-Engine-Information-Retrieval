078ea896ffdab902fe688ec89673b7201a49e3d2
Jaymin <James.patel37@gmail.com>
2016-11-15 11:34:02 -0800

    workspace

---
+++ Workspace/SearchEngine/src/search/Search.java
@@ -0,0 +1,34 @@
+package search;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Search {
+
+	public static void main(String args[]) {
+		
+		try {
+
+			String content = "This is the content to write into file";
+
+			File file = new File("C:/Users/Jaymin/Desktop/temp.txt");
+
+			// if file doesnt exists, then create it
+			if (!file.exists()) {
+				file.createNewFile();
+			}
+
+			FileWriter fw = new FileWriter(file.getAbsoluteFile());
+			BufferedWriter bw = new BufferedWriter(fw);
+			bw.write(content);
+			bw.close();
+
+			System.out.println("Done");
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
